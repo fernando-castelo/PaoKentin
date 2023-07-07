dae9ad394e01a0eeccd20ecb6506ba133f28ecba
fernando-castelo <nando.castelobranco24@hotmail.com>
2023-07-07 14:55:34 -0300

    First commit

---
+++ src/test/java/com/example/PaoKentin/PaoKentinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaoKentin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaoKentinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
